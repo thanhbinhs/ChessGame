835518ce112923306b9cc95bdb9f223d293ee911
ThanhBinh <thanhbinhsmart@gmail.com>
2023-08-31 22:59:07 +0700

    add

---
+++ ChessGame/header.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef HEADER_H
+#define HEADER_H
+
+#include<SFML/Graphics.hpp>
+#include<iostream>
+
+const int SCREEN_WIDTH = 860;
+const int SCREEN_HEIGHT = 640;
+
+
+#endif // !HEADER_H
+
+
+
--- ChessGame/main.cpp
+++ ChessGame/main.cpp
@@ -1,9 +1,19 @@
-#include<SFML/Graphics.hpp>
-#include<iostream>
+#include "header.h"
 
 using namespace std;
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode());
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH,SCREEN_HEIGHT),"SFML",sf::Style::Default);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+	}
+
+	window.display();
 
 }
