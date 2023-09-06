b0b58fd63160a01557e9f3b9b73d5a3674052518
ThanhBinh <thanhbinhsmart@gmail.com>
2023-09-06 23:19:32 +0700

    result 6/9

--- ChessGame/header.h
+++ ChessGame/header.h
@@ -4,9 +4,11 @@
 
 #include<SFML/Graphics.hpp>
 #include<iostream>
+#include<stack>
 
-const int SCREEN_WIDTH = 840;
-const int SCREEN_HEIGHT = 680;
+const int SCREEN_WIDTH = 1040;
+const int SCREEN_HEIGHT = 690;
+static sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML", sf::Style::Close);
 
 const sf::Vector2f sizeBtn(80, 80);
 
